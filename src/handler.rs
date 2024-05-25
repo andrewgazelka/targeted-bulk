46db1b6ecdcb041d5201a6144cb4c4f33556c7dd
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-05-24 22:55:42 -0500

    feat: support non-EntityId target types in TargetedEvents

--- src/handler.rs
+++ src/handler.rs
@@ -1,25 +1,26 @@
 use evenio::{
+    entity::EntityId,
     fetch::{Fetcher, Single},
     handler::HandlerParam,
     query::Query,
 };
 
-use crate::{TargetedEvents, TargetedId};
+use crate::{RayonThreadAssignable, TargetedEvents, ThreadPinned};
 
 // todo: can we remove 'static bound?
 // todo: how evenio use TypeId without 'static bound?
 #[derive(HandlerParam)]
-pub struct TargetedReader<'a, E: 'static, Q: Query + 'static> {
-    events: Single<'a, &'static mut TargetedEvents<E>>,
+pub struct TargetedReader<'a, E: 'static, Q: Query + 'static, T: 'static = EntityId> {
+    events: Single<'a, &'static mut TargetedEvents<E, T>>,
     query: Fetcher<'a, Q>,
 }
 
-impl<'a, E: 'static, Q: Query + 'static> TargetedReader<'a, E, Q> {
+impl<'a, E: 'static, Q: Query + 'static> TargetedReader<'a, E, Q, EntityId> {
     // todo: is there a way to avoid this?
     #[allow(clippy::mut_mut)]
     pub fn drain_par<'b, F>(&'b mut self, process: F)
     where
-        F: Fn(TargetedId, E, Q::Item<'b>) + Send + Sync,
+        F: Fn(ThreadPinned<EntityId>, E, Q::Item<'b>) + Send + Sync,
         E: Send + Sync,
         Q::Item<'b>: Send + Sync,
     {
@@ -27,7 +28,7 @@ impl<'a, E: 'static, Q: Query + 'static> TargetedReader<'a, E, Q> {
         let query = &mut self.query;
 
         events.drain_par(|target, data| {
-            let item = unsafe { query.get_unchecked(target.inner()) };
+            let item = unsafe { query.get_unchecked(*target.inner()) };
 
             let Ok(item) = item else {
                 return;
@@ -39,16 +40,19 @@ impl<'a, E: 'static, Q: Query + 'static> TargetedReader<'a, E, Q> {
 }
 
 #[derive(HandlerParam)]
-pub struct TargetedWriter<'a, E: 'static> {
-    events: Single<'a, &'static mut TargetedEvents<E>>,
+pub struct TargetedWriter<'a, E: 'static, T: 'static = EntityId> {
+    events: Single<'a, &'static mut TargetedEvents<E, T>>,
 }
 
-impl<'a, E: 'static> TargetedWriter<'a, E> {
-    pub fn push_exclusive(&mut self, target: TargetedId, data: E) {
-        self.events.0.push_exclusive(target.inner(), data);
+impl<'a, E: 'static, T: 'static> TargetedWriter<'a, E, T> {
+    pub fn push_exclusive(&mut self, target: T, data: E)
+    where
+        T: RayonThreadAssignable,
+    {
+        self.events.0.push_exclusive(target, data);
     }
 
-    pub fn push_shared(&self, target: TargetedId, data: E) {
+    pub fn push_shared(&self, target: ThreadPinned<T>, data: E) {
         self.events.0.push_shared(target, data);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,25 @@
 #![feature(allocator_api)]
 
-use std::{
-    cell::UnsafeCell,
-    fmt::{Debug, Formatter},
-    marker::PhantomData,
-    thread::ThreadId,
-};
+use std::{cell::UnsafeCell, fmt::Debug, marker::PhantomData, thread::ThreadId};
 
 use evenio::{component::Component, entity::EntityId};
 
 pub mod handler;
 
-#[derive(PartialEq, Eq, Copy, Clone)]
-pub struct TargetedId {
-    data: EntityId,
-    /// So [`TargetedId`] is not [`Send`] nor [`Sync`].
+#[derive(PartialEq, Eq, Copy, Clone, Debug)]
+pub struct ThreadPinned<T> {
+    data: T,
+    /// So [`ThreadPinned`] is not [`Send`] nor [`Sync`].
     _marker: PhantomData<*const ()>,
 }
 
-impl Debug for TargetedId {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("TargetedId").field(&self.data).finish()
-    }
-}
-
-impl TargetedId {
+impl<T> ThreadPinned<T> {
     #[must_use]
-    pub const fn inner(self) -> EntityId {
-        self.data
+    pub const fn inner(&self) -> &T {
+        &self.data
     }
 
-    const fn new(data: EntityId) -> Self {
+    const fn new(data: T) -> Self {
         Self {
             data,
             _marker: PhantomData,
@@ -38,18 +27,46 @@ impl TargetedId {
     }
 }
 
-fn get_rayon_thread_index(id: EntityId) -> usize {
-    id.index().0 as usize % rayon::current_num_threads()
+/// # Safety
+/// Must be a pure function that returns the same value for the same input.
+pub unsafe trait RayonThreadAssignable {
+    fn get_rayon_thread_index(&self) -> usize;
+}
+
+macro_rules! impl_rayon_thread_assignable_for_primitive {
+    ($($t:ty),*) => {
+        $(
+            // this is fine, we just want a consistent mapping
+            #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+            unsafe impl RayonThreadAssignable for $t {
+                fn get_rayon_thread_index(&self) -> usize {
+                    get_rayon_thread_index(*self as usize)
+                }
+            }
+        )*
+    };
+}
+
+impl_rayon_thread_assignable_for_primitive!(u8, u16, u32, u64, usize, i8, i16, i32, i64, isize);
+
+unsafe impl RayonThreadAssignable for EntityId {
+    fn get_rayon_thread_index(&self) -> usize {
+        get_rayon_thread_index(self.index().0 as usize)
+    }
+}
+
+fn get_rayon_thread_index(id: usize) -> usize {
+    id % rayon::current_num_threads()
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct LocalEvents<E> {
+pub struct LocalEvents<E, T> {
     pinned_thread_id: Option<ThreadId>,
-    targets: Vec<EntityId>,
+    targets: Vec<T>,
     events: Vec<E>,
 }
 
-impl<E> LocalEvents<E> {
+impl<E, T> LocalEvents<E, T> {
     const EMPTY: Self = Self {
         targets: Vec::new(),
         events: Vec::new(),
@@ -71,20 +88,20 @@ impl<E> LocalEvents<E> {
 }
 
 #[derive(Debug, Component)]
-pub struct TargetedEvents<E> {
-    locals: Box<[UnsafeCell<LocalEvents<E>>]>,
+pub struct TargetedEvents<E, T = EntityId> {
+    locals: Box<[UnsafeCell<LocalEvents<E, T>>]>,
 }
 
-unsafe impl<E> Send for TargetedEvents<E> {}
-unsafe impl<E> Sync for TargetedEvents<E> {}
+unsafe impl<E, T> Send for TargetedEvents<E, T> {}
+unsafe impl<E, T> Sync for TargetedEvents<E, T> {}
 
-impl<E> Default for TargetedEvents<E> {
+impl<E, T> Default for TargetedEvents<E, T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<E> TargetedEvents<E> {
+impl<E, T> TargetedEvents<E, T> {
     #[must_use]
     pub fn new() -> Self {
         rayon::current_num_threads();
@@ -114,8 +131,11 @@ impl<E> TargetedEvents<E> {
             .all(|local| local.is_empty())
     }
 
-    pub fn push_exclusive(&mut self, target: EntityId, data: E) {
-        let idx = get_rayon_thread_index(target);
+    pub fn push_exclusive(&mut self, target: T, data: E)
+    where
+        T: RayonThreadAssignable,
+    {
+        let idx = target.get_rayon_thread_index();
 
         let locals = self
             .locals
@@ -128,14 +148,14 @@ impl<E> TargetedEvents<E> {
 
     /// # Panics
     /// Panics if the target's assigned thread index does not match the current thread index.
-    pub fn push_shared(&self, target: TargetedId, data: E) {
+    pub fn push_shared(&self, target: ThreadPinned<T>, data: E) {
         let local = self.get_current_local();
         let local = unsafe { &mut *local.get() };
         local.push(target.data, data);
     }
 
     /// # Safety
-    fn get_current_local(&self) -> &UnsafeCell<LocalEvents<E>> {
+    fn get_current_local(&self) -> &UnsafeCell<LocalEvents<E, T>> {
         let current_thread_index = rayon::current_thread_index().expect("not in a rayon thread");
         let local = self.get_local(current_thread_index);
 
@@ -147,7 +167,7 @@ impl<E> TargetedEvents<E> {
         local
     }
 
-    fn get_local(&self, index: usize) -> &UnsafeCell<LocalEvents<E>> {
+    fn get_local(&self, index: usize) -> &UnsafeCell<LocalEvents<E, T>> {
         self.locals
             .get(index)
             .expect("thread index out of bounds; did you create multiple rayon ThreadPools?")
@@ -157,7 +177,7 @@ impl<E> TargetedEvents<E> {
     #[allow(clippy::needless_pass_by_ref_mut)]
     pub fn drain_par<F>(&mut self, process: F)
     where
-        F: Fn(TargetedId, E) + Send + Sync,
+        F: Fn(ThreadPinned<T>, E) + Send + Sync,
         E: Send + Sync,
     {
         rayon::broadcast(|_| {
@@ -168,20 +188,20 @@ impl<E> TargetedEvents<E> {
             let events = local.events.drain(..);
 
             targets.zip(events).for_each(|(target, data)| {
-                let target = TargetedId::new(target);
+                let target = ThreadPinned::new(target);
                 process(target, data);
             });
         });
     }
 }
 
-impl<E> Default for LocalEvents<E> {
+impl<E, T> Default for LocalEvents<E, T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<E> LocalEvents<E> {
+impl<E, T> LocalEvents<E, T> {
     #[must_use]
     const fn new() -> Self {
         Self {
@@ -199,7 +219,7 @@ impl<E> LocalEvents<E> {
         self.targets.is_empty()
     }
 
-    fn push(&mut self, target: EntityId, data: E) {
+    fn push(&mut self, target: T, data: E) {
         self.targets.push(target);
         self.events.push(data);
     }
